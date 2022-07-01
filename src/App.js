c1924e84629d428ef83b0d59b246bbf77fa412ff
Nafiu Mubashir <85135332+Nafiu-Mubashir@users.noreply.github.com>
2022-07-01 16:41:06 +0100

    chore: add dummy text (#1)

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Virtual Classroom App
         </a>
       </header>
     </div>
