d6bc9cfbe403284dae5f16c71a21f1a71c34d02d
Nafiu Mubashir <mubashirnafiu@gmail.com>
2022-07-04 10:37:12 +0100

    Chore: I created the component folder

---
+++ src/components/Student_Login.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Student_Login() {
+  return (
+    <div>Student_Login</div>
+  )
+}
+
+export default Student_Login
---
+++ src/components/Student_SignUp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Student_SignUp() {
+  return (
+    <div>Student_SignUp</div>
+  )
+}
+
+export default Student_SignUp
